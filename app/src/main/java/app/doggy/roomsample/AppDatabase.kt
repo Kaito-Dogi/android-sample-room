4e8694d4203099e1ea0b678a81268c9f3a7459a0
Kaito-Dogi <kendobu0405@gmail.com>
2022-02-28 00:46:37 +0900

    clean: AppDatabaseのコメントを整理

--- app/src/main/java/app/doggy/roomsample/AppDatabase.kt
+++ app/src/main/java/app/doggy/roomsample/AppDatabase.kt
@@ -4,30 +4,35 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import kotlinx.coroutines.CoroutineScope
 
-@Database(entities = arrayOf(User::class), version = 1)
+@Database(entities = [User::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
+    // 複数のインスタンスが同時にデータベースにアクセスすることを防ぐために、シングルトンとする。
     companion object {
-        // Singleton prevents multiple instances of database opening at the
-        // same time.
+
+        /**
+         * @Volatile
+         * マルチスレッド処理では、処理の効率化のために、同じフィールドの値を、それぞれのスレッドが個別にキャッシュすることがある。
+         * そのため、同じフィールドにも関わらず、スレッドによって参照値が異なる危険性がある。
+         * よって、Volatile修飾子を用いて、フィールドの値がキャッシュされることを抑制し、値の不整合を回避する。
+         */
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
+        // INSTANCEがnullでない時、INSTANCEを返す。
+        // INSTANCEがnullの時、AppDatabaseをインスタンス化してINSTANCEに代入する。
         fun getDatabase(context: Context): AppDatabase {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "room_sample_database"
+                    "sample_room_database"
                 ).build()
                 INSTANCE = instance
 
-                // return instance
+                // instanceを返す。
                 instance
             }
         }
